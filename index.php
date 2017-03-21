ef5c66e4d7610ad4763509c0ab8a0292f745e9f7
alumno <alumno>
2017-03-20 21:30:11 -0300

    Ejercico 6 funcionando correctamente Manera 1

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-echo "Hola Murndo";
+//para mostrar array usar var_dump
+$promedio = 0;
+$lista = array(rand(1,10),rand(1,10),rand(1,10),rand(1,10),rand(1,10)); // manera 1
+
+var_dump($lista);
+for ($i=0; $i < 5; $i++) { 
+    $promedio += $lista[$i];
+}
+$promedio = $promedio / 5;
+if ($promedio < 6) {
+    echo "<br>Promedio : ".$promedio." < 6";
+}
+else if ($promedio > 6) {
+    echo "<br>Promedio : ".$promedio." > 6";
+}
+else {
+    echo "<br>Promedio : ".$promedio." = 6";
+}
 ?>
